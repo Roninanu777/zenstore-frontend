ed71ecb1a0a2e2a59c0921be0660105ec3057c8a
@Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2022-03-02 10:22:00 +0530

    init directories

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-// postcss.config.js
-module.exports = ({ env }) => ({
+module.exports = {
   plugins: {
-    tailwindcss,
-    autoprefixer,
+    tailwindcss: {},
+    autoprefixer: {},
   },
-});
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
-import "./App.css";
+import "./tailwind.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <main>
+      <div className="flex items-center justify-center w-screen h-screen">
+        <div className="w-20 h-20 bg-slate-500"></div>
+        <div className="w-20 h-20 bg-red-500"></div>
+      </div>
+    </main>
+  );
 }
-
 export default App;
