fbfc636589b9f2a905208860fda51dab1a528023
@Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2022-03-06 22:52:50 +0530

    adding redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
+import appStore from "./store/appStore";
 import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={appStore}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
